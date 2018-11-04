a5b78eb87aa27638751ece732a714da4765d8b8b
shebin007 <shebinkp7@gmail.com>
2018-11-04 20:23:11 +0530

    my first Wordpress Plugin Intialization

---
+++ attendence.php
@@ -0,0 +1,14 @@
+< <?php
+/**
+ * @package attendence
+ */
+/*
+  Plugin Name: Attendence
+  Plugin URI :https://github.com/shebin007/Myfirstplugin
+  Description: This is my first Plugin
+  Version : 1.0.0
+  Author : Shebin KP
+  Author URI: https://github.com/shebin007/Myfirstplugin
+  License: GPLv2 or later
+  Text Domain: attendence
+*/
